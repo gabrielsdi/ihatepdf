fbc2c24577f5f4ac3c6cf42e0062d6ab9642bdaf
gabrielsdi <gabrielsdi18@gmail.com>
2026-09-14 07:53:17 -0300

    feat: fix vercel issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ihatepdf/',
+  base: process.env.VERCEL ? '/' : '/ihatepdf/',
   optimizeDeps: {
     include: ['pdfjs-dist'],
   },
